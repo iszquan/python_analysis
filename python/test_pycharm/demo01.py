990174fa8ef7737e38aa4fbfbbf02e718338c5da
iszquan <1398987697@qq.com>
2024-06-27 15:33:58 +0800

    first_submit

---
+++ python/test_pycharm/demo01.py
@@ -0,0 +1,12 @@
+# --------    -------    --------
+# -*- coding: utf-8 -*-
+# @Time : 2024/6/11 16:22
+# @Author : ZQ
+# @File : demo01
+# @Project : pydocx.py
+# @FileSummarize ： 
+# --------    -------    --------
+from time import gmtime
+
+str = 2691
+print(gmtime(str))
